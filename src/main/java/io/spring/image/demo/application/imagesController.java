f8c1b79572fa28f31825ed3041ac54d69a8c725e
Luz Pisca Pisca <geovana.luz@estudante.ifmt.edu.br>
2026-04-23 10:54:54 -0400

    23.04

---
+++ src/main/java/io/spring/image/demo/application/imagesController.java
@@ -0,0 +1,44 @@
+package io.spring.image.demo.application;
+
+import io.spring.image.demo.domain.entity.Image;
+import io.spring.image.demo.domain.enums.ImageExtension;
+import io.spring.image.demo.domain.service.ImageService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/upload")
+@Slf4j
+@RequiredArgsConstructor
+public class imagesController {
+    private final ImageService service;
+    @PostMapping
+    public ResponseEntity save(@RequestParam("file")  MultipartFile file,
+                                      @RequestParam("name")String name,
+                                      @RequestParam("tags") List<String> tags
+    ) throws IOException {
+        log.info("Recebendo tentativa de upload do arquivo: {}", file.getOriginalFilename());
+        log.info("Content Type:{} ", file.getContentType());
+        log.info("Media Type:{} ", MediaType.valueOf(file.getContentType()));
+
+        Image image = Image.builder()
+                .name(name)
+                .tags(String.join(",", tags))
+                .size(file.getSize())
+                .extension(ImageExtension.valueOf(MediaType.valueOf(file.getContentType())))
+                .file(file.getBytes())
+                .build();
+        service.save(image);
+        return ResponseEntity.ok().build();
+    }
+}
